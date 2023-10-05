3af7d3d35339936471f2836156d4f400a56e35cd
Ivan <ivan.mondragon.sotres@gmail.com>
2023-10-04 18:29:41 -0600

    (ADD) Separate styles are added in the UserListComponent

--- src/User/UserList.js
+++ src/User/UserList.js
@@ -1,9 +1,11 @@
+import './user.css';
+
 /* User list component */
 export function UserList({ user }) {
 
     return (<div>
         <h1>Usuarios</h1>
-        <ul>
+        <ul className="userCard">
             <li>Nombre: {user.name}</li>
             <li>Apellido: {user.lastname}</li>
             <li>Edad: {user.age}</li>
