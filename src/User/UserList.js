48d7b801e9378fd5a1e74be4e70f203369eddb8f
Ivan <ivan.mondragon.sotres@gmail.com>
2023-10-04 19:56:25 -0600

    (ADD) Added request to users API with fetch

--- src/User/UserList.js
+++ src/User/UserList.js
@@ -1,9 +1,30 @@
 import './user.css';
 
 /* User list component */
-export function UserList({ user }) {
 
-    return (<div>
+const getUsers = () => {
+    const userUrl = 'https://api.devdicio.net:8444/v1/sec_dev_interview?limit=2'
+    const requestOptions = {
+        method: 'GET', // Método HTTP (GET en este caso)
+        headers: {
+            'xc-token': 'J38b4XQNLErVatKIh4oP1jw9e_wYWkS86Y04TMNP'
+        }
+    };
+
+    return fetch(userUrl, requestOptions)
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            return json
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+export function UserList({ user }) {
+    getUsers()
+    return (<>
         <h1>Usuarios</h1>
         <ul className="userCard">
             <li>Nombre: {user.name}</li>
@@ -11,7 +32,7 @@ export function UserList({ user }) {
             <li>Edad: {user.age}</li>
             <li>Email: {user.email}</li>
         </ul>
-    </div>
+    </>
     )
 
 }
--- src/index.js
+++ src/index.js
@@ -4,17 +4,27 @@ import { UserList } from "./User/UserList";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
-const user = {
-    name: "John",
-    lastname: "Doe",
-    age: 25,
-    email: "sdjfalskdjf"
-};
+const user =     {
+    "id": 33,
+    "nombre": "Jorge Alberto",
+    "apellidoPaterno": "Martinez",
+    "apellidoMaterno": "De la Barrera",
+    "edad": 34,
+    "email": "apune.i@gmail.com",
+    "fechaNac": "1994-05-28",
+    "datos": {
+        "calle": " ",
+        "numero": " ",
+        "colonia": " ",
+        "delegacion": " ",
+        "estado": "Ciudad de México",
+        "cp": "undefined",
+        "imagen": "https://scontent.fmex10-1.fna.fbcdn.net/v/t1.0-9/10407680_10204100063686"
+    }
+}
 
 root.render(
     <div>
-        <UserList user={user} />
-        <UserList user={user} />
         <UserList user={user} />
     </div>
 );
