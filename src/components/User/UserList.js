d8bc646030557c218cd4156f3d98418536c69fc6
Ivan <ivan.mondragon.sotres@gmail.com>
2023-10-04 23:40:05 -0600

    (MOD) Refactor user list componente with axios library

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -1,37 +1,44 @@
+import { useEffect, useState } from 'react';
 import './user.css';
+import axios from 'axios';
 
 /* User list component */
-
-const getUsers = () => {
-    const userUrl = 'https://api.devdicio.net:8444/v1/sec_dev_interview?limit=2'
-    const requestOptions = {
-        method: 'GET', // Método HTTP (GET en este caso)
-        headers: {
-            'xc-token': 'J38b4XQNLErVatKIh4oP1jw9e_wYWkS86Y04TMNP'
-        }
-    };
-
-    return fetch(userUrl, requestOptions)
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-            return json
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
-
 export function UserList({ user }) {
-    getUsers()
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        getUsers()
+    }, []);
+
+    const getUsers = async () => {
+        const userUrl = 'https://api.devdicio.net:8444/v1/sec_dev_interview?limit=20'
+        const requestOptions = {
+            method: 'GET', // Método HTTP (GET en este caso)
+            headers: {
+                'xc-token': 'J38b4XQNLErVatKIh4oP1jw9e_wYWkS86Y04TMNP'
+            }
+        };
+
+        const response = await axios.get(userUrl, requestOptions);
+        console.log(response.data);
+        setUsers(response.data);
+    }
+
     return (<>
         <h1>Usuarios</h1>
-        <ul className="userCard">
-            <li>Nombre: {user.name}</li>
-            <li>Apellido: {user.lastname}</li>
-            <li>Edad: {user.age}</li>
-            <li>Email: {user.email}</li>
-        </ul>
+        {/* map de users */}
+        <div className="container">
+            {users.map((user) => (
+                <div className="card" key={user.id}>
+                    <img src={user.datos.imagen} alt="Avatar" style={{ width: "100%" }} />
+                    <div className="container">
+                        <h4><b>{user.nombre} {user.apellidoPaterno} {user.apellidoMaterno}</b></h4>
+                        <p>{user.email}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
     </>
     )
 
