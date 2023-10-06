99c5ebf8f0196c558e180285e153c580e780cd28
Ivan <ivan.mondragon.sotres@gmail.com>
2023-10-05 22:50:20 -0600

    (ADD) Added search filter to list user component

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -1,13 +1,30 @@
 import "./user.css";
 import { useUsers } from "../../hooks/useUsers";
+import { useState } from "react";
 
 /* User list component */
 export function UserList() {
+  const [query, setQuery] = useState("");
   const { users } = useUsers();
+
+  function handleOnChanges(event) {
+    console.log(event.target.value);
+    setQuery(event.target.value.trim());
+  }
 
   return (
     <>
       <h1>Usuarios</h1>
+      <div className="input-group input-group-lg">
+        <span className="input-group-text" id="inputGroup-sizing-lg">
+          Buscar
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleOnChanges}
+        />
+      </div>
       <div className="container">
         <div className="table-responsive">
           <table className="table table-bordered">
@@ -25,21 +42,31 @@ export function UserList() {
               </tr>
             </thead>
             <tbody className="table-group-divider">
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user?.id}</td>
-                  <td>{user?.nombre}</td>
-                  <td>{user?.apellidoPaterno}</td>
-                  <td>{user?.apellidoMaterno}</td>
-                  <td>{user?.edad}</td>
-                  <td>{user?.email}</td>
-                  <td>{user?.fechaNac}</td>
-                  <td>{user?.datos?.calle}</td>
-                  <td>
-                    {/* <img className="img-fluid img-thumbnail" src={user.datos.imagen} alt="Avatar" /> */}
-                  </td>
-                </tr>
-              ))}
+              {users
+                .filter((user) => {
+                  if (query === "") {
+                    return user;
+                  } else if (
+                    user?.nombre?.toLowerCase().includes(query?.toLowerCase())
+                  ) {
+                    return user;
+                  }
+                })
+                .map((user) => (
+                  <tr key={user.id}>
+                    <td>{user?.id}</td>
+                    <td>{user?.nombre}</td>
+                    <td>{user?.apellidoPaterno}</td>
+                    <td>{user?.apellidoMaterno}</td>
+                    <td>{user?.edad}</td>
+                    <td>{user?.email}</td>
+                    <td>{user?.fechaNac}</td>
+                    <td>{user?.datos?.calle}</td>
+                    <td>
+                      {/* <img className="img-fluid img-thumbnail" src={user?.datos?.imagen} alt="Avatar" /> */}
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
