8ea059cf28c3a9d389a4a85738f8446d6cfde6e4
Ivan <ivan.mondragon.sotres@gmail.com>
2023-10-06 12:13:38 -0600

    (ADD) Added routes to user app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { UserList } from "./components/User/UserList"
-import { AddUser } from "./components/User/AddUser";
+import { Outlet } from "react-router-dom";
+import { Menu } from "./components/Menu";
 
 export function App() {
-    return (
-        <>
-            <AddUser />
-            <UserList />
-        </>
-    )
+  return (
+    <>
+      <Menu />
+      <div className="vh-100">
+        <h1 className="text-center">Aplicación de Usuarios</h1>
+        <Outlet />
+      </div>
+    </>
+  );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { UserList } from "./components/User/UserList";
+import { AddUser } from "./components/User/AddUser";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "users",
+        element: <UserList />,
+      },
+      {
+        path: "/users/add",
+        element: <AddUser />,
+      },
+    ],
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
